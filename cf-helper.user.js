3876ddf2fe1a2e945beb7afda9c7126a74542a74
renzhamin <renzhamin@gmail.com>
2023-04-26 00:36:35 +0600

    fix: clipboard api now copies to source area on keypress 's'

--- cf-helper.user.js
+++ cf-helper.user.js
@@ -10,56 +10,57 @@
 // @run-at       document-start
 // ==/UserScript==
 
-let problemPageFontSize="1.8em";
-let submitKey='s';
-let solutionPageKey='g';
-let friendsolutionPageKey='f';
-let contestRankingPage='R';
+let problemPageFontSize = "1.8em"
+let submitKey = "s"
+let solutionPageKey = "g"
+let friendsolutionPageKey = "f"
+let contestRankingPage = "R"
 
-let arr=location.pathname.replace('problemset/problem','contest').split('/') , n=arr.length;
+let arr = location.pathname.replace("problemset/problem", "contest").split("/"),
+    n = arr.length
 
-(function (){
-    window.addEventListener('load',function(){
-        if(arr[n-1]=="submit"){
-            submitPage();
+;(function () {
+    window.addEventListener("load", function () {
+        if (arr[n - 1] == "submit") {
+            submitPage()
         } else {
-            problemStatementPage();
+            problemStatementPage()
         }
-    });
-})();
+    })
+})()
 
+function submitPage() {
+    // select problem from saved value
+    document.querySelector('select[name="submittedProblemIndex"]').value =
+        GM_getValue("pid", "A")
+    window.scroll(0, 172)
 
-
-
-
-function submitPage(){
-    document.addEventListener('keydown', function (event) {
+    document.addEventListener("keydown", function (event) {
+        // copy clipboard content to source code area and click submit button
         if (event.key === submitKey) {
-            document.querySelector('input[value="Submit"]').click();
+            navigator.clipboard.readText().then((clipText) => {
+                document.getElementById("sourceCodeTextarea").value = clipText
+                document.querySelector('input[value="Submit"]').click()
+            })
         }
-    });
-    document.querySelector('select[name="submittedProblemIndex"]').value=GM_getValue('pid','A');
-    window.scroll(0,172);
-    navigator.clipboard.readText().then(clipText =>
-        document.getElementById("sourceCodeTextarea").value=clipText);
+    })
 }
 
-
-
-function problemStatementPage(){
-    document.querySelector('.problem-statement').style.fontSize=problemPageFontSize;
-    let url=`/${arr[1]}/${arr[2]}`;
-    GM_setValue('pid',arr[n-1]);
-    window.scroll(0,172);
-    document.addEventListener('keydown', function (event) {
+function problemStatementPage() {
+    document.querySelector(".problem-statement").style.fontSize =
+        problemPageFontSize
+    let url = `/${arr[1]}/${arr[2]}`
+    GM_setValue("pid", arr[n - 1])
+    window.scroll(0, 172)
+    document.addEventListener("keydown", function (event) {
         if (event.key === submitKey) {
-            window.open(`${url}/submit`);
-        } else if (event.key === solutionPageKey){
-            window.open(`${url}/status/${arr[n-1]}`);
-        } else if (event.key === friendsolutionPageKey){
-            window.open(`${url}/status/${arr[n-1]}/?friends=on`);
+            window.open(`${url}/submit`)
+        } else if (event.key === solutionPageKey) {
+            window.open(`${url}/status/${arr[n - 1]}`)
+        } else if (event.key === friendsolutionPageKey) {
+            window.open(`${url}/status/${arr[n - 1]}/?friends=on`)
         } else if (event.key === contestRankingPage) {
-            window.open(`${url}/standings/friends/true`);
+            window.open(`${url}/standings/friends/true`)
         }
-    });
+    })
 }
